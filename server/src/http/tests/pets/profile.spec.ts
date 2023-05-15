7b05ccc3d14467d97ca875a6423de701bf305596
Abner <kif1@gmail.com>
2023-05-15 11:29:35 -0300

    final testes e2e

--- server/src/http/tests/pets/profile.spec.ts
+++ server/src/http/tests/pets/profile.spec.ts
@@ -2,8 +2,9 @@ import request from "supertest";
 import { app } from "../../../app";
 import { afterAll, beforeAll, describe, expect, it } from "vitest";
 import { createAndAuthenticateOrganization } from "utils/create-and-authenticate-organization";
+import { randomUUID } from "crypto";
 
-describe('Filter Pet  (e2e)', () => {
+describe('Get a Pet (e2e)', () => {
   beforeAll(async () => {
     await app.ready()
   })
@@ -12,10 +13,12 @@ describe('Filter Pet  (e2e)', () => {
     await app.close()
   })
 
-  it('Shold be able to filter a pet by you caracteristics ', async () => {
-    const { idOrganization, token } = await createAndAuthenticateOrganization(app)
+  it('Shold be able to get a pet ', async () => {
+    const { idOrganization, token } = await createAndAuthenticateOrganization(app);
+    const idPet = randomUUID();
 
     await request(app.server).post('/pets').set('Authorization', `Bearer ${token}`).send({
+      id: idPet,
       name: 'Vira Lata',
       description: 'Vira Lata',
       age: 'filhote',
@@ -28,23 +31,10 @@ describe('Filter Pet  (e2e)', () => {
       photos: ['https://localhost:4000.png', 'https://localhost:4000.png']
     })
 
-    await request(app.server).post('/pets').set('Authorization', `Bearer ${token}`).send({
-      name: 'Pudou',
-      description: 'Pudou',
-      age: 'filhote',
-      size: 'pequeno',
-      energy_level: 'Baixa',
-      dependency_level: 'Baixa',
-      environment: 'grande',
-      requirements: ['ambiente amplo', 'ambiente receptivo'],
-      organization_id: idOrganization,
-      photos: ['https://localhost:4000.png', 'https://localhost:4000.png']
-    })
-
-    const petResponse = await request(app.server).get('/pets/nearby?filtered=Pernambuco&city=Olinda&page=1&size=pequeno&dependency_level=baixa').set('Authorization', `Bearer ${token}`).send({})
+    const petResponse = await request(app.server).get('/pets/idPet').set('Authorization', `Bearer ${token}`).send({})
     expect(petResponse.statusCode).toEqual(200);
     expect(petResponse.body.pets).toEqual(expect.objectContaining({
-      name: 'Pudou'
+      name: 'Vira Lata'
     }))
   })
 })
