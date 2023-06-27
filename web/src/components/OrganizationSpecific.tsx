be51514d3ee35cee05f764774f7e2bcc54f15676
Abner Santos <abner.kif1@gmail.com>
2023-06-26 21:47:48 -0300

    create page organization

--- web/src/components/OrganizationSpecific.tsx
+++ web/src/components/OrganizationSpecific.tsx
@@ -10,6 +10,24 @@ export function OrganizationSpecific(){
           <div className="w-[85%] mx-auto flex flex-col gap-8">
             <h1 className="text-5xl text-blue font-bold">Java Script Academy</h1>
             <span className="text-xl text-blue">Academia de Pets</span>
+            <div className="w-full flex flex-wrap gap-x-8 gap-y-0">
+              <div className="w-fit flex items-center gap-2">
+                <strong className="text-xl text-blue font-bold">Email:</strong>
+                <span className="text-lg text-blue">abner.kif1@gmail.com</span>
+              </div>
+              <div className="w-fit flex items-center gap-2">
+                <strong className="text-xl text-blue font-bold">Telefone:</strong>
+                <span className="text-lg text-blue">(81) 984421742</span>
+              </div>
+              <div className="w-fit flex items-center gap-2">
+                <strong className="text-xl text-blue font-bold">Cep:</strong>
+                <span className="text-lg text-blue">53250-519</span>
+              </div>
+              <div className="w-fit flex items-center gap-2">
+                <strong className="text-xl text-blue font-bold">Endereço:</strong>
+                <span className="text-lg text-blue">Rua do meio, 123 , Boa viagem, Recife - PE </span>
+              </div>
+            </div>
             <h1 className="text-5xl text-blue font-bold">Veja os pets desta organização</h1>
             <PetsView />
             <Link href="" className="w-full bg-green p-4 text-2xl text-white font-bold rounded-2xl flex justify-center items-center gap-4">
